23953d594d84870f0cc4e41f534c7a42637d9eea
Chris Birchall <chris.birchall@47deg.com>
2022-06-14 09:41:05 +0100

    Fix mappings and add descriptions

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -14,7 +14,7 @@ vim.keymap.set("i", "<Up>", "<Nop>")
 vim.keymap.set("i", "<Down>", "<Nop>")
 
 -- Move between windows without having to press Ctrl-W
-vim.keymap.set("", "<C-h>", "C-w>h")
-vim.keymap.set("", "<C-j>", "C-w>j")
-vim.keymap.set("", "<C-k>", "C-w>k")
-vim.keymap.set("", "<C-l>", "C-w>l")
+vim.keymap.set("", "<C-h>", "<C-w>h")
+vim.keymap.set("", "<C-j>", "<C-w>j")
+vim.keymap.set("", "<C-k>", "<C-w>k")
+vim.keymap.set("", "<C-l>", "<C-w>l")
--- .config/nvim/lua/metals_config.lua
+++ .config/nvim/lua/metals_config.lua
@@ -16,22 +16,22 @@ local m = require('metals')
 
 vim.keymap.set("n", "gD", vim.lsp.buf.definition, { desc = "Go to definition" })
 vim.keymap.set("n", "K", vim.lsp.buf.hover)
-vim.keymap.set("n", "gi", vim.lsp.buf.implementation)
-vim.keymap.set("n", "gr", vim.lsp.buf.references)
-vim.keymap.set("n", "gds", vim.lsp.buf.document_symbol)
-vim.keymap.set("n", "gws", vim.lsp.buf.workspace_symbol)
-vim.keymap.set("n", "<leader>cl", vim.lsp.codelens.run)
-vim.keymap.set("n", "<leader>sh", vim.lsp.buf.signature_help)
-vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename)
-vim.keymap.set("n", "<leader>f", vim.lsp.buf.formatting)
-vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action)
-vim.keymap.set("n", "<leader>ws", m.hover_worksheet)
-vim.keymap.set("n", "<leader>aa", vim.diagnostic.setqflist) -- all workspace diagnostics
-vim.keymap.set("n", "<leader>ae", function() vim.diagnostic.setqflist({severity = "E"}) end) -- all workspace errors
-vim.keymap.set("n", "<leader>aw", function() vim.diagnostic.setqflist({severity = "W"}) end) -- all workspace warnings
-vim.keymap.set("n", "<leader>d", vim.diagnostic.setloclist) -- buffer diagnostics only
-vim.keymap.set("n", "[c", function() vim.diagnostic.goto_prev({wrap = false}) end)
-vim.keymap.set("n", "]c", function() vim.diagnostic.goto_next({wrap = false}) end)
+vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { desc = "Show implementations" })
+vim.keymap.set("n", "gr", vim.lsp.buf.references, { desc = "Show references" })
+vim.keymap.set("n", "gds", vim.lsp.buf.document_symbol, { desc = "List symbols in document" })
+vim.keymap.set("n", "gws", vim.lsp.buf.workspace_symbol, { desc = "Search for symbol in workspace" })
+vim.keymap.set("n", "<leader>cl", vim.lsp.codelens.run, { desc = "Open CodeLens action menu" })
+vim.keymap.set("n", "<leader>sh", vim.lsp.buf.signature_help, { desc = "Show signature help" })
+vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename symbol" })
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.formatting, { desc = "Format the current buffer" })
+vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Open code action menu" })
+vim.keymap.set("n", "<leader>ws", m.hover_worksheet, { desc = "Show info about current line of worksheet" })
+vim.keymap.set("n", "<leader>aa", vim.diagnostic.setqflist, { desc = "List all workspace diagnostics" })
+vim.keymap.set("n", "<leader>ae", function() vim.diagnostic.setqflist({severity = "E"}) end, { desc = "List all workspace errors" })
+vim.keymap.set("n", "<leader>aw", function() vim.diagnostic.setqflist({severity = "W"}) end, { desc = "List all workspace warnings" })
+vim.keymap.set("n", "<leader>d", vim.diagnostic.setloclist, { desc = "List all diagnostics for current buffer" })
+vim.keymap.set("n", "[c", function() vim.diagnostic.goto_prev({wrap = false}) end, { desc = "Jump to previous diagnostic in buffer" })
+vim.keymap.set("n", "]c", function() vim.diagnostic.goto_next({wrap = false}) end, { desc = "Jump to next diagnostic in buffer" })
 
 local metals_config = require("metals").bare_config()
 
