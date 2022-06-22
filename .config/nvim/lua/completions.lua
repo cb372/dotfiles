c28c92ff5c5913a60b4ef7e79ff4169685be50b5
Chris Birchall <chris.birchall@47deg.com>
2022-06-22 12:10:34 +0100

    Remove custom mapping for C-c in nvim-cmp
    We want to leave C-c mapped to <Esc> so it both closes the completion
    menu and exits insert mode.

--- .config/nvim/lua/completions.lua
+++ .config/nvim/lua/completions.lua
@@ -21,7 +21,6 @@ cmp.setup({
     ['<C-b>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
-    ['<C-c>'] = cmp.mapping.abort(),
     ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
     ["<Tab>"] = function(fallback)
       if cmp.visible() then
