f38a1c0d56eae739b2767d8a3ec1bed1fc29c601
Chris Birchall <chris.birchall@47deg.com>
2022-12-15 18:02:50 +0000

    Fix weird completion menu behaviour

--- .config/nvim/lua/completions.lua
+++ .config/nvim/lua/completions.lua
@@ -21,7 +21,7 @@ cmp.setup({
     ['<C-b>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    ['<CR>'] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
     ["<Tab>"] = function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
