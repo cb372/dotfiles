40f02c032585713fafbb0fafe94fca78f7d44e96
Chris Birchall <chris.birchall@47deg.com>
2023-02-02 17:46:40 +0000

    Specify treesitter grammars

--- .config/nvim/lua/treesitter_config.lua
+++ .config/nvim/lua/treesitter_config.lua
@@ -1,6 +1,31 @@
-require'nvim-treesitter.configs'.setup {
+require('nvim-treesitter.configs').setup {
   -- A list of parser names, or "all"
-  ensure_installed = "all",
+  ensure_installed = {
+    "bash",
+    "diff",
+    "dockerfile",
+    "git_rebase",
+    "gitattributes",
+    "gitcommit",
+    "go",
+    "hcl",
+    "hocon",
+    "html",
+    "java",
+    "javascript",
+    "jq",
+    "json",
+    "lua",
+    "python",
+    "ruby",
+    "rust",
+    "scala",
+    "smithy",
+    "sql",
+    "toml",
+    "typescript",
+    "vim"
+  },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = true
